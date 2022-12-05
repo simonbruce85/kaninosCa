72c7dd08a3e4ca387fa180b5c9fc7a157e2c90e7
Simon Bruce <simonbrucepuma@gmail.com>
2022-12-04 23:11:07 -0600

    Login page styling

--- src/frontend/src/pages/Login.jsx
+++ src/frontend/src/pages/Login.jsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../auth/AuthContext";
 import { auth } from "../firebase";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input } from "antd";
+import KaninosLogo from "../assets/kaninoslogo.png";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -12,63 +15,82 @@ const Login = () => {
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError('')
+    // e.preventDefault();
+    setError("");
     try {
-      setPersistence(auth, browserSessionPersistence)
+      setPersistence(auth, browserSessionPersistence);
       await logIn(email, password);
       navigate("/");
     } catch (error) {
       console.log(error);
-      setError(error.message)
+      setError(error.message);
     }
+  };
+
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
   };
 
   return (
     <>
-      <div className="w-full h-screem">
-        <img
+      <div className="w-full h-screem ">
+        {/* <img
           className="block absolute w-full h-full object-cover"
           src="https://cdn.pixabay.com/photo/2015/11/03/12/58/dalmatian-1020790_960_720.jpg"
           alt="background"
-        />
-        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        /> */}
+        <div className=" fixed top-0 left-0 w-full bg-gray-300 h-screen"></div>
         <div className="fixed w-full px-4 py-24 z-50">
-          <div className="max-w-[450px] md:h-[600px] mx-auto bg-black/75 text-white">
-            <div className="max-w-[320px] mx-auto py-16">
-              <h1 className="text-3x font-bold text-white">Log In</h1>
-              {error ? <p className="p-3 bg-red-400 my-2">{error}</p>:null}
-              <form
-                onSubmit={handleSubmit}
-                className="w-full flex flex-col py-4"
+          <div className="max-w-[450px] md:h-[600px] mx-auto bg-white text-white">
+            <div className="max-w-[320px] mx-auto py-8 flex flex-col justify-center">
+              <div className="flex justify-center">
+                <img className=""  src={KaninosLogo}/>
+                </div>
+              <h1 className=" text-black">Log In</h1>
+              {error ? <p className="p-3 bg-red-400 my-2">{error}</p> : null}
+              <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{
+                  remember: true,
+                }}
+                onFinish={handleSubmit}
               >
-                <input
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="p-3 my-2 bg-gray-700 rounded"
-                  type="email"
-                  placeholder="Email"
-                  autoComplete="email"
-                />
-                <input
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="p-3 my-2 bg-gray-700 rounded"
-                  type="password"
-                  placeholder="Password"
-                  autoComplete="current-password"
-                />
-                <button className="bg-gradient-to-r from-[#F06CA6] via-[#F58352] to-[#F06CA6] py-3 my-6 rounded-full font-bold hover:scale-105">
-                  Ingresar
+                <Form.Item
+                  name="username"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your Username!",
+                    },
+                  ]}
+                >
+                  <Input
+                    onChange={(e) => setEmail(e.target.value)}
+                    prefix={<UserOutlined className="site-form-item-icon" />}
+                    placeholder="Username"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your Password!",
+                    },
+                  ]}
+                >
+                  <Input
+                    onChange={(e) => setPassword(e.target.value)}
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                  />
+                </Form.Item>
+                  <button className="bg-gradient-to-r w-full from-[#39b89e] via-[#39b89e] to-[#39b89e] p-3 my-2 rounded-lg font-bold hover:scale-105">
+                  Log in
                 </button>
-                <div className="flex justify-between items-center text-sm text-gray-600">
-                  
-                  <p> Need Help?</p>
-                </div>
-                <div className="py-4">
-                  <p>Test Credentials</p>
-                  <p>Email: simon@simon.com</p>
-                  <p>Password: simon12</p>
-                </div>
-              </form>
+              </Form>
             </div>
           </div>
         </div>
