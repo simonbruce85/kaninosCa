26920cb2f0994d40329068c585acf3bf7d805a8d
Simon Bruce <simonbrucepuma@gmail.com>
2023-01-27 03:36:51 -0600

    login page

--- src/frontend/src/pages/Login.jsx
+++ src/frontend/src/pages/Login.jsx
@@ -6,6 +6,7 @@ import { auth } from "../firebase";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Checkbox, Form, Input } from "antd";
 import KaninosLogo from "../assets/kaninoslogoBackground.png";
+import video1 from "../assets/videoDog.gif";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -33,76 +34,77 @@ const Login = () => {
 
   return (
     <>
-      <div className="w-full h-screem ">
+      <div className="w-full h-screen flex justify-center bg-gray-300 items-center">
         {/* <img
           className="block absolute w-full h-full object-cover"
           src="https://cdn.pixabay.com/photo/2015/11/03/12/58/dalmatian-1020790_960_720.jpg"
           alt="background"
         /> */}
-        <div className=" fixed top-0 left-0 w-full bg-gray-300 h-screen"></div>
-        <div className="fixed w-full px-4 py-24 z-50">
-          <div className="max-w-[450px] md:h-[600px] mx-auto bg-white text-white">
-            <div className="max-w-[320px] mx-auto py-8 flex flex-col justify-center">
-              <div className="flex justify-center">
-                <img className="" src={KaninosLogo} />
-              </div>
-              <h1 className=" text-black">Log In</h1>
-              {error ? <p className="p-3 bg-red-400 my-2">{error}</p> : null}
-              <Form
-                name="normal_login"
-                className="login-form"
-                initialValues={{
-                  remember: true,
-                }}
-                onFinish={handleSubmit}
-              >
-                <Form.Item
-                  name="username"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please input your Username!",
-                    },
-                  ]}
+        <div className=" w-full max-w-[80%] sm:max-w-[50%] md:max-w-[50%]  lg:w-[50%] flex shadow-md bg-white rounded-xl">
+          <img src={video1} className=" max-w-[40%] hidden xl:block rounded-l-xl" />
+          <div className="w-full flex justify-center">
+            <div className="max-w-[450px] mx-[8px] md:w-full md:max-w-1/2 bg-white text-white">
+              <div className="max-w-[320px] mx-auto py-8 flex flex-col justify-center">
+                <div className="flex justify-center">
+                  <img className="" src={KaninosLogo} />
+                </div>
+                <h1 className=" text-black">Log In</h1>
+                {error ? <p className="p-3 bg-red-400 my-2">{error}</p> : null}
+                <Form
+                  name="normal_login"
+                  className="login-form"
+                  initialValues={{
+                    remember: true,
+                  }}
+                  onFinish={handleSubmit}
                 >
-                  <Input
-                    onChange={(e) => setEmail(e.target.value)}
-                    prefix={<UserOutlined className="site-form-item-icon" />}
-                    placeholder="Username"
-                  />
-                </Form.Item>
-                <Form.Item
-                  name="password"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please input your Password!",
-                    },
-                  ]}
-                >
-                  <Input
-                    onChange={(e) => setPassword(e.target.value)}
-                    prefix={<LockOutlined className="site-form-item-icon" />}
-                    type="password"
-                    placeholder="Password"
-                  />
-                </Form.Item>
-                <button className="bg-gradient-to-r w-full from-[#39b89e] via-[#39b89e] to-[#39b89e] p-3 my-2 rounded-lg font-bold hover:scale-105">
-                  Log in
-                </button>
-              </Form>
-              <div className="text-black">
-                <p>
-                  Username = simon@simon.com
-                </p>
-                <p>
-                  password = kaninos12345
-                </p>
+                  <Form.Item
+                    name="username"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your Username!",
+                      },
+                    ]}
+                  >
+                    <Input
+                      onChange={(e) => setEmail(e.target.value)}
+                      prefix={<UserOutlined className="site-form-item-icon" />}
+                      placeholder="Username"
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your Password!",
+                      },
+                    ]}
+                  >
+                    <Input
+                      onChange={(e) => setPassword(e.target.value)}
+                      prefix={<LockOutlined className="site-form-item-icon" />}
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </Form.Item>
+                  <button className="bg-gradient-to-r w-full from-[#fce4fc] via-[#fce4fc] to-[#f8d7f8] p-3 my-2 rounded-lg font-bold hover:scale-105">
+                    Log in
+                  </button>
+                </Form>
+                <div className="text-black">
+                  <p>
+                    Username = simon@simon.com
+                  </p>
+                  <p>
+                    password = kaninos12345
+                  </p>
+                </div>
               </div>
-
             </div>
-
           </div>
+
         </div>
       </div>
     </>
